7a5ebfcef24173a2ba4763bbf4b5aec14883893e
Zheng-Kun <zheng-kun@foxmail.com>
2018-07-15 18:24:08 +0800

    提交项目
    脚印二期项目

---
+++ scripts/formctl.js
@@ -0,0 +1,138 @@
+//
+
+//每天9-21，12小时 - 弹性时间 3 小时  = 9小时
+var creat_route = document.getElementById('creat-route');
+
+creat_route.addEventListener('click',getFormInfromation);
+
+//获取表单内容
+function getFormInfromation(ev){
+
+    var travel_province = document.getElementsByName('travel-province')[0];
+    var travel_days = document.getElementsByName('travel-days')[0];
+    var travel_hobby = document.getElementsByName('travel-hobby')[0];
+    var travel_mode = document.getElementsByName('travel-mode')[0];
+
+    var Information = {};
+    Information.province = travel_province.options[travel_province.selectedIndex].value;
+    Information.days = parseInt(travel_days.options[travel_days.selectedIndex].value);
+    Information.hobby = travel_hobby.options[travel_hobby.selectedIndex].value;
+    Information.mode = travel_mode.options[travel_mode.selectedIndex].value;
+    Information.hotel = document.getElementById('hotel-input').value;
+    if(Information.hotel.length <= 0){
+        alert("请填输入住酒店");
+    }else{
+        creatRoute(Information);
+    }
+    return false;
+}
+//生成路径
+function creatRoute(infObject) {
+    //存储路径的二维数组
+    var routeArr = [];
+    //按天数循环
+    for(let j = 1;j<=infObject.days;j++){
+        routeArr = creatDayRoute(routeArr,infObject);
+    }
+
+    console.log(routeArr);
+
+    creatRouteOnDom(routeArr);
+}
+
+//生成一天的路径
+function creatDayRoute(routeArr,infObject){
+    //当前的点
+    var currentPlace = infObject.hotel;
+    var dayRouteArr = [infObject.hotel];
+    dayRouteArr.push()
+    // routeArr.push(dayRouteArr);
+    //当前的时间
+    var currentTime = 9;
+    while(currentTime<20){
+
+        //权重最大的点
+        var maxWeight = {
+            name:null,
+            weight:0,
+            time:0,
+        }
+        //寻找权重最高的景点
+        for(let i = 0;i<scenicChongQing.length;i++){
+            //如果景点存在，跳过
+            if(isExist(routeArr,dayRouteArr,scenicChongQing[i].name)){
+                continue;
+            }
+            //由于异步问题，该信息暂时不做使用
+            var points = [
+                { keyword: currentPlace,city:'重庆' },
+                { keyword: scenicChongQing[i].name,city:'重庆' }
+            ];
+            var weight = scenicWeight(scenicChongQing[i],infObject,currentPlace,currentTime);
+            if(weight>maxWeight.weight){
+                maxWeight.name = scenicChongQing[i].name;
+                maxWeight.weight = weight;
+                maxWeight.time = scenicChongQing[i].time;
+            }
+        }
+        dayRouteArr.push(maxWeight.name);
+        currentTime += maxWeight.time;
+    }
+    routeArr.push(dayRouteArr);
+    return routeArr;
+}
+
+//检查当前景点是否已经被加入攻略
+function isExist(routeArr,dayRouteArr,currentScenic){
+    for(let j = 0;j<routeArr.length;j++){
+        for(let i = 0;i < routeArr[j].length; i++){
+            if(routeArr[j][i] == currentScenic){
+                return true;
+            }
+        }
+    }
+    for(let i = 0;i<dayRouteArr.length;i++){
+        if(dayRouteArr[i] == currentScenic){
+            return true;
+        }
+    }
+    return false;
+}
+
+
+//计算景点权重
+function scenicWeight(scenicObj,infObject,currentPlace,currentTime){
+
+    //暂时假定到达景点的时间为1小时
+    var roadTime = 1;
+
+    //计算权重,
+    var weight = 0;
+
+    //景点类型权重 权重20
+    if(scenicObj.type == infObject.hobby){
+        weight = weight + 20;
+    }
+
+    //时间段权重，权重10
+    if(currentTime>18 && scenicObj.playtime == "night"){
+        weight += 10;
+    }
+    //如果不是最佳游玩时间段，减去20权重
+    if(currentTime<18 && scenicObj.playtime == "night"){
+        weight -= 20;
+    }
+
+    //星级权重，权重20
+    weight += scenicObj.score*4;
+
+    //游玩时间占比权重（路上的时间与游玩时间的占比）权重10
+    weight += (scenicObj.time/(scenicObj.time + roadTime)).toFixed(2)*10;
+
+    //如果剩余时间不够游玩该景点，减去20权重
+    if(currentTime + scenicObj.time > 22){
+        weight -= 20;
+    }
+
+    return weight;
+}
---
+++ scripts/mapAPI.js
@@ -0,0 +1,132 @@
+//输入提示
+AMap.plugin(['AMap.Autocomplete'],autoComplete);
+function autoComplete(){
+    var autoCom = new AMap.Autocomplete({
+        city:"重庆",
+        dataType:"all",
+        citylimit:true,
+        input:"hotel-input",
+
+    });
+    // autoCom.search("喜来登酒店",searchAuto);
+}
+
+var timeArr = [];
+
+function getTimeDistence(points){
+/*
+
+    var map = new AMap.Map("myMap", {
+        resizeEnable: true,
+    });
+*/
+    // console.log("Amap插件加载完成！");
+    var driving = new AMap.Driving({
+        // 驾车路线规划策略，AMap.DrivingPolicy.LEAST_TIME是最快捷模式
+        // map:map,
+        policy: AMap.DrivingPolicy.LEAST_TIME,
+        //是否显示路况
+        showTraffic:true,
+        //显示结果列表的容器Id或元素
+        // panel:'MyRoute',
+        //自动调整路径显示位置及大小
+        autoFitView:true,
+    });
+
+        //根据起终点（途经点最多六个）实现驾车路线规划
+    driving.search(points,getResult);
+    function getResult(status, result){
+        //未出错时，result即是对应的路线规划方案
+        return timeDistence = (result.routes[0].time/3600).toFixed(2);
+    }
+}
+
+function creatRouteOnDom(routeArr){
+    var result_box = document.getElementById("result");
+    result_box.innerHTML = "";
+    creatTextRoute(routeArr);
+    creatAMapRoute(routeArr);
+}
+
+function creatTextRoute(routeArr){
+    var result_box = document.getElementById("result");
+    var text_route = document.createElement("div");
+    text_route.id = "text-route";
+    var text_ul = document.createElement("ul");
+    for(let i = 0;i <routeArr.length;i++){
+        var text_li = document.createElement("li");
+        var routetext = "第" + (i+1) + "天：";
+        for(let j = 0 ;j <routeArr[i].length;j++){
+            routetext  = routetext + routeArr[i][j] + "->" ;
+        }
+        routetext = routetext + routeArr[i][0];
+        text_li.innerText = routetext;
+        text_ul.appendChild(text_li);
+    }
+    text_route.appendChild(text_ul);
+    result_box.appendChild(text_route);
+}
+
+function creatAMapRoute(routeArr){
+    var result_box = document.getElementById("result");
+    for(let i = 0;i <routeArr.length;i++){
+        var AmapResult = document.createElement("div");
+        AmapResult.classList.add("AmapResult");
+
+        //文字信息
+        var AmapResultH = document.createElement("h2");
+        AmapResultH.classList.add("AmapResultH");
+        var routetext = "第" + (i+1) + "天：";
+        for(let j = 0 ;j <routeArr[i].length;j++){
+            routetext  = routetext + routeArr[i][j] + "->" ;
+        }
+        routetext = routetext + routeArr[i][0];
+        AmapResultH.innerText = routetext;
+
+        //aMap生成的文字路径规划
+        var AmapTextRoute = document.createElement("div");
+        AmapTextRoute.id = "Amap-text-route" + i;
+        AmapTextRoute.classList.add("Amap-text-route");
+        var AmapMapRoute = document.createElement("div");
+        AmapMapRoute.id = "Amap-map-route" + i;
+        AmapMapRoute.classList.add("Amap-map-route");
+
+        //aMap生成的地图路径规划
+        result_box.appendChild(AmapResultH);
+        AmapResult.appendChild(AmapTextRoute);
+        AmapResult.appendChild(AmapMapRoute);
+
+        var map = new AMap.Map(AmapMapRoute, {
+            resizeEnable: false,
+            // mapStyle: 'amap://styles/whitesmoke', //设置地图的显示样式
+            isHotspot:true,//是否开启地图热点和标注的hover效果
+            zoomEnable:true,//地图是否可缩放
+            scrollWheel:false,//地图是否可通过鼠标滚轮缩放浏览
+        });
+
+        var driving = new AMap.Driving({
+            // 驾车路线规划策略，AMap.DrivingPolicy.LEAST_TIME是最快捷模式
+            map:map,
+            policy: AMap.DrivingPolicy.LEAST_TIME,
+            //是否显示路况
+            showTraffic:true,
+            //显示结果列表的容器Id或元素
+            panel:AmapTextRoute.id,
+            //自动调整路径显示位置及大小
+            autoFitView:true,
+        });
+        //生成路径对象
+        var points = [];
+        for(let j = 0;j<routeArr[i].length; j++){
+            points.push({keyword:routeArr[i][j],city:'重庆'});
+        }
+        points.push({keyword:routeArr[i][0],city:'重庆'});
+
+        //根据起终点（途经点最多六个）实现驾车路线规划
+        driving.search(points,getResult);
+        function getResult(status, result){
+            //未出错时，result即是对应的路线规划方案
+        }
+        result_box.appendChild(AmapResult);
+    }
+}
